6f1a513a0cfe664c3f0e4c5f923866d20bb561d1
Sasha Golovach <sashagolovach@Sashas-MacBook-Pro.local>
2018-10-27 06:20:39 +0300

    For Misha

--- TornadoServer/Server.py
+++ TornadoServer/Server.py
@@ -2,15 +2,15 @@ import tornado.escape
 import tornado.ioloop
 import tornado.web
 import os
-import api
+#import api
 
 
-feed = api.get_feed()
+#feed = api.get_feed()
 
 
 class FeedHandler(tornado.web.RequestHandler):
     def get(self):
-        response = next(feed).encode()
+        response = ''  # next(feed).encode()
         self.write(response)
 
 
@@ -20,6 +20,6 @@ class Server():
             (r"/feed", FeedHandler)])
 
     def listen(self):
-        port = int(os.environ.get("PORT", 8888))
+        port = int(os.environ.get("PORT", 80))
         self.server.listen(port)
         tornado.ioloop.IOLoop.instance().start()
